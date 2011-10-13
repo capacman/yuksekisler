48d7269f843fc6ede261719d8320b29d3c63fd3d
capacman <achalil@gmail.com>
2011-10-13 22:31:58 +0000

    definitions view bitti

--- dojo/src/main/webapp/resources/js/yuksekisler/Definitions.js
+++ dojo/src/main/webapp/resources/js/yuksekisler/Definitions.js
@@ -5,3 +5,69 @@
  * Time: 2:19 AM
  * To change this template use File | Settings | File Templates.
  */
+dojo.provide('yuksekisler.Definitions');
+
+dojo.declare('yuksekisler.Definitions', [dijit._Widget,dijit._Templated], {
+    templateString:dojo.cache('yuksekisler.Definitions', '../../../templates/definitions_template.html'),
+    widgetsInTemplate:true,
+    categoryStore:null,
+    brandStore:null,
+    postCreate:function() {
+        this.categoryGrid = new dojox.grid.DataGrid({
+            query:{},
+            selectionMode:'single',
+            id:'categoryGrid',
+            rowCount:10,
+            store:new dojo.data.ObjectStore({objectStore: this.categoryStore}),
+            autoHeight:5,
+            structure:
+                [
+                    {name:'Name',field:'name',width:'auto'},
+                    {name:'Description',field:'description',width:'auto'}
+                ],
+            onRowContextMenu:dojo.hitch(this, this.onRowContextMenu)
+        });
+        this.categoryGridContainer.set('content', this.categoryGrid);
+        this.categoryGrid.startup();
+
+        this.brandGrid = new dojox.grid.DataGrid({
+            query:{},
+            selectionMode:'single',
+            id:'brandGrid',
+            rowCount:10,
+            store:new dojo.data.ObjectStore({objectStore: this.brandStore}),
+            autoHeight:5,
+            structure:
+                [
+                    {name:'Name',field:'name',width:'auto'},
+                    {name:'Description',field:'description',width:'auto'}
+                ],
+            onRowContextMenu:dojo.hitch(this, this.onRowContextMenu)
+        });
+        this.brandGridContainer.set('content', this.brandGrid);
+        this.brandGrid.startup();
+        this.inherited(arguments);
+    },
+    onBrand:function() {
+        this.brandStore.put(this.brandForm.get('value')).then(dojo.hitch(this, function() {
+            this.brandForm.reset();
+            this.brandGrid._refresh();
+        }));
+    },
+    onCategory:function() {
+        this.categoryStore.put(this.categoryForm.get('value')).then(dojo.hitch(this, function() {
+            this.categoryForm.reset();
+            this.categoryGrid._refresh();
+        }));
+    },
+    onRowContextMenu:function(e) {
+        this.gridMenu.bindDomNode(e.grid.domNode);
+        this.contextMenuClicked = function(d) {
+            e.grid.store.deleteItem(e.grid.getItem(e.rowIndex));
+            e.grid.store.save();
+        }
+    },
+    contextMenuClicked:function(e) {
+
+    }
+});
